bddb1c5bc7e9bb391608acb95074d9df20907f99
Vladimir Hooke <g@hooke.dev>
2023-05-30 10:51:19 +0200

    refactor + templates

---
+++ src/ui/src/main.js
@@ -0,0 +1,10 @@
+if (localStorage.getItem('token') || localStorage.getItem('email')) {
+  app.form.init();
+  setInterval(() => {
+    const backup = app.form.getFormData();
+    if (!backup) return;
+    app.form.setBackup(backup);
+  }, 5000);
+} else {
+  app.auth.requestAuthorization();
+}
